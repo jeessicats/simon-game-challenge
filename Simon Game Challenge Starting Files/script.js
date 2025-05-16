27dd6c4ec8282d3bda7c314aa35e41b4325ec929
jeessicats <jeessicats@gmail.com>
2025-05-16 17:45:57 -0300

    step 1 to 5

--- Simon Game Challenge Starting Files/script.js
+++ Simon Game Challenge Starting Files/script.js
@@ -1,1 +1,33 @@
-alert("JavaScript está funcionando!");
+let gamePattern = [];
+
+let userClickedPattern = [];
+
+let buttonColors = ["red", "blue", "green", "yellow"];
+
+let randomChosenColour;
+
+$(".btn").click(function() {
+    let userChosenColour = $(this).attr("id");
+    userClickedPattern.push(userChosenColour);
+
+    // Playing the sound
+    playSound(userChosenColour);
+});
+
+
+function playSound(name) {
+    let audio = new Audio("sounds/" + name + ".mp3");
+    audio.play();
+}
+
+function nextSequence() {
+    let randomNumber = Math.floor(Math.random() * 4);
+    randomChosenColour = buttonColors[randomNumber];
+    gamePattern.push(randomChosenColour);
+
+    // Flashing the button
+    $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+
+    // Playing the sound
+    playSound(randomChosenColour);
+}
